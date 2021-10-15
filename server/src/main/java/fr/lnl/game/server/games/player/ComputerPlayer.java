77a2800a78016e5bd49042c100fbefc0d2356a5c
Katchan <neveuthomas5@gmail.com>
2021-10-15 18:52:44 +0200

    add players basis

--- server/src/main/java/fr/lnl/game/server/games/player/ComputerPlayer.java
+++ server/src/main/java/fr/lnl/game/server/games/player/ComputerPlayer.java
@@ -4,7 +4,7 @@ import fr.lnl.game.server.games.weapon.Weapon;
 import fr.lnl.game.server.utils.Point;
 
 public class ComputerPlayer extends AbstractPlayer{
-    public ComputerPlayer(int id, Point position, int ernergy, Weapon weapon, boolean shieldDeploy) {
-        super(id, position, ernergy, weapon, shieldDeploy);
+    public ComputerPlayer(int id, Point position, int energy, Weapon weapon, boolean shieldDeploy) {
+        super(id, position, energy, weapon, shieldDeploy);
     }
 }
--- server/src/main/java/fr/lnl/game/server/games/player/HumanPlayer.java
+++ server/src/main/java/fr/lnl/game/server/games/player/HumanPlayer.java
@@ -4,7 +4,7 @@ import fr.lnl.game.server.games.weapon.Weapon;
 import fr.lnl.game.server.utils.Point;
 
 public class HumanPlayer extends AbstractPlayer{
-    public HumanPlayer(int id, Point position, int ernergy, Weapon weapon, boolean shieldDeploy) {
-        super(id, position, ernergy, weapon, shieldDeploy);
+    public HumanPlayer(int id, Point position, int energy, Weapon weapon, boolean shieldDeploy) {
+        super(id, position, energy, weapon, shieldDeploy);
     }
 }
