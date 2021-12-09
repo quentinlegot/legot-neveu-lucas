af5cbf80c1bccd2bb9bb2fc6092d09d40a45a2c7
Quentin Legot <legotquentin@gmail.com>
2021-12-09 17:33:14 +0100

    add start bash and cmd files

---
+++ startTerminal.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+gradle --version
+gradle :client:run --args="terminal human dps computerS tank"
---
+++ startWindow.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+gradle --version
+gradle :client:run --args="window human dps computerS tank"
